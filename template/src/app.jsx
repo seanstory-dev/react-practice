85be618c9d9ddc568428ffb9c1773346eae20645
seanstory-dev <seanstory.dev@gmail.com>
2022-02-03 16:05:42 +0900

    Add template

---
+++ template/src/app.jsx
@@ -0,0 +1,13 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>hello react :)</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
