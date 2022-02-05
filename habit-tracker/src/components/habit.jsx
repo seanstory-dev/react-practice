0de0f9ec4f8a7c43cded354ddadc68ba36ae9275
seanstory-dev <seanstory.dev@gmail.com>
2022-02-05 14:47:35 +0900

    Add habits component and refactor habit component

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
-
-  handleIncrement = (e) => {
-    console.log(e);
-    // setState로 전체적인 state를 업데이트해줘야 리액트가 render 함수를 자동으로 호출할 수 있다.
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
   };
 
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
 
   render() {
+    const { name, count } = this.props.habit;
+
     return (
       <li className='habit'>
-        <span className='habit-name'>Reading</span>
-        <span className='habit-count'>{this.state.count}</span>
+        <span className='habit-name'>{name}</span>
+        <span className='habit-count'>{count}</span>
         <button className='habit-button habit-increase' onClick={this.handleIncrement}>
           <i className='fas fa-plus-square'></i>
         </button>
         <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
           <i className='fas fa-minus-square'></i>
         </button>
-        <button className='habit-button habit-delete'>
+        <button className='habit-button habit-delete' onClick={this.handleDelete}>
           <i className='fas fa-trash'></i>
         </button>
       </li>
